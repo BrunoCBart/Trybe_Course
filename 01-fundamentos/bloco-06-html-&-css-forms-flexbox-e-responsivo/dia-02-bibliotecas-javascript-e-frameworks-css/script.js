9e4e690cf4f055dc6eb33fa68c229a37541232aa
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-08-20 15:58:42 -0300

    Atualiza bloco 6

--- 01-fundamentos/bloco-06-html-&-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
+++ 01-fundamentos/bloco-06-html-&-css-forms-flexbox-e-responsivo/dia-02-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,7 +1,4 @@
 const selecStates = document.querySelector('#estados-brasil')
-const startDate = document.querySelector('#data-inicio')
-const submitBtn = document.querySelector('#submit')
-
 
 const states = {
   'AC': 'Acre',
@@ -47,102 +44,122 @@ function makeStateEl() {
   }
 }
 
-function startDateIsValid(date) {
-  let objDate,
-    mSeconds,
-    day,
-    month,
-    year;
+// function startDateIsValid(date) {
+//   let objDate,
+//     mSeconds,
+//     day,
+//     month,
+//     year;
 
-  if (date.length !== 10) {
-    console.log(1)
-    return false;
-  }
+//   if (date.length !== 10) {
+//     console.log(1)
+//     return false;
+//   }
 
-  if (date.substring(2, 3) !== '/' || date.substring(5, 6) !== '/') {
-    console.log(2)
-    return false;
-  }
+//   if (date.substring(2, 3) !== '/' || date.substring(5, 6) !== '/') {
+//     console.log(2)
+//     return false;
+//   }
 
-  day = parseInt(date.substring(0, 2))
-  month = parseInt(date.substring(3, 5) - 1);
-  year = parseInt(date.substring(6, 10 - 0));
+//   day = parseInt(date.substring(0, 2))
+//   month = parseInt(date.substring(3, 5) - 1);
+//   year = parseInt(date.substring(6, 10 - 0));
 
-  if (year < 0) return false
+//   if (year < 0) return false
 
-  mSeconds = new Date(year, month, day).getTime();
+//   mSeconds = new Date(year, month, day).getTime();
 
-  objDate = new Date()
-  objDate.setTime(mSeconds)
+//   objDate = new Date()
+//   objDate.setTime(mSeconds)
 
-  if (objDate.getFullYear() !== year ||
-    objDate.getMonth() !== month ||
-    objDate.getDate() !== day) {
-    console.log(typeof year, typeof month, typeof day)
-    console.log(3)
-    console.log(objDate.getFullYear())
-    console.log(objDate.getMonth())
-    console.log(objDate.getDate())
-    return false;
-  }
-  return true;
-}
+//   if (objDate.getFullYear() !== year ||
+//     objDate.getMonth() !== month ||
+//     objDate.getDate() !== day) {
+//     console.log(typeof year, typeof month, typeof day)
+//     console.log(3)
+//     console.log(objDate.getFullYear())
+//     console.log(objDate.getMonth())
+//     console.log(objDate.getDate())
+//     return false;
+//   }
+//   return true;
+// }
 
-function checkDate() {
-  // define date string to test 
-  const date = document.getElementById('data-inicio').value;
-  // check date and print message 
-  if (!startDateIsValid(date)) {
-    alert('Data inválida');
-  }
-}
+// function checkDate() {
+//   // define date string to test 
+//   const date = document.getElementById('data-inicio').value;
+//   // check date and print message 
+//   if (!startDateIsValid(date)) {
+//     alert('Data inválida');
+//   }
+// }
 
 
 
-submitBtn.addEventListener('click', (e) => {
-  e.preventDefault()
-  checkDate()
-  if (checkRequired()) displayForm()
-})
+// submitBtn.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   // checkDate()
+//   // if (checkRequired()) displayForm()
+// })
 
-function checkRequired() {
-  const required = document.querySelectorAll('input[required]')
-  for (let req of required) {
-    if (req.value === '') return false
-  }
-  return true
-}
+// function checkRequired() {
+//   const required = document.querySelectorAll('input[required]')
+//   for (let req of required) {
+//     if (req.value === '') return false
+//   }
+//   return true
+// }
 
-function createSection() {
-  const section = document.createElement('section')
-  return section;
-}
+// function createSection() {
+//   const section = document.createElement('section')
+//   return section;
+// }
 
-function createDiv() {
-  const div = document.createElement('div')
-  return div
-}
+// function createDiv() {
+//   const div = document.createElement('div')
+//   return div
+// }
 
-function createP() {
-  const p = document.createElement('p')
-  return p
-}
+// function createP() {
+//   const p = document.createElement('p')
+//   return p
+// }
 
-function displayForm() {
-  const inputText = document.querySelectorAll('input[type="text"]')
-  const oldSection = document.querySelector('section')
-  if (oldSection) oldSection.remove()
-  const section = createSection()
-  const div = createDiv()
-  section.classList.add('form-result')
-  inputText.forEach(input => {
-    const p = createP()
-    p.innerText = input.value
-    div.appendChild(p)
-  })
-  section.appendChild(div)
-  document.body.appendChild(section)
-}
+// function displayForm() {
+//   const inputText = document.querySelectorAll('input[type="text"]')
+//   const oldSection = document.querySelector('section')
+//   if (oldSection) oldSection.remove()
+//   const section = createSection()
+//   const div = createDiv()
+//   section.classList.add('form-result')
+//   inputText.forEach(input => {
+//     const p = createP()
+//     p.innerText = input.value
+//     div.appendChild(p)
+//   })
+//   section.appendChild(div)
+//   document.body.appendChild(section)
+// }
+
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
 
 window.onload = () => {
   makeStateEl()
