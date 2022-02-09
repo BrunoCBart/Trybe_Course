c7a901944de6cd96930c91e927bfe4bc0653a23f
Bruno Bartolomeu <bruno.cbartlomeu@gmail.com>
2022-02-09 16:30:07 -0300

    ex_01

---
+++ 03-back-end/bloco-22-introducao-ao-node/dia-02-nodejs-fluxo-assincrono/ex_01.js
@@ -0,0 +1,9 @@
+const func = (a, b ,c) => {
+  return new Promise((resolve, reject) => {
+    if(typeof a !== 'number' 
+    || typeof b !== 'number'
+    || typeof c !== 'number') reject("Informe apenas números")
+    const result = (a + b) * c
+    result < 50 ? "Valor muito baixo": resolve(result)
+  })
+}
