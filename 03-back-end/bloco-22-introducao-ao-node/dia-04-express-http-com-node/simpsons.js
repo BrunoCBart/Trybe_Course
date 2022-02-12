8a59971c04bf22903006f23f64bcf06c94fd0c1f
Bruno Bartolomeu <bruno.cbartlomeu@gmail.com>
2022-02-12 16:02:02 -0300

    ex_08

--- 03-back-end/bloco-22-introducao-ao-node/dia-04-express-http-com-node/simpsons.js
+++ 03-back-end/bloco-22-introducao-ao-node/dia-04-express-http-com-node/simpsons.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const fs = require('fs')
+const simpsonsUtils = require('./simpsons-utils');
 
 const app = express()
 
-const simpsonsArr = JSON.parse(fs.readFileSync('simpsons.json').toString())
+app.use(express.json())
 
-app.get('/simpsons', (req, res) => {
-  res.status(200).json(simpsonsArr)
+// const simpsonsArr = JSON.parse(fs.readFileSync('simpsons.json').toString())
+
+app.get('/simpsons', async (req, res) => {
+  const simpsons = await simpsonsUtils.getSimpsons()
+  res.status(200).json(simpsons)
 })
 
-app.get('/simpsons/:id', (req, res) => {
+//test
+app.get('/simpsons/:id', async (req, res) => {
   const { id } = req.params
-  const character = simpsonsArr.find((simp) => simp.id === id)
+  const simpsons = await simpsonsUtils.getSimpsons()
+  const character = simpsons.find((simp) => simp.id === id)
   if (!character) return res.status(404).json({ message: 'simpson not found' })
   res.status(200).json(character)
+})
+
+app.post('/simpsons', async (req, res) => {
+  const { id, name} = req.body
+  const simpsons = await simpsonsUtils.getSimpsons()
+  const characterExists = simpsons.find((simp) => simp.id === id)
+  if (characterExists) return res.status(409).json({message:'id alredy exists'})
+  simpsons.push({id, name})
+  await simpsonsUtils.setSimpsons(simpsons)
+  res.status(204).end()
 })
 
 app.listen(2000, () => {
