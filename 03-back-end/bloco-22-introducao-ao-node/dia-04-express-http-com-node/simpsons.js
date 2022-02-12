9627bc7f66138c2f8c0502dc1344239572949885
Bruno Bartolomeu <bruno.cbartlomeu@gmail.com>
2022-02-12 16:22:22 -0300

    ex_bonus

--- 03-back-end/bloco-22-introducao-ao-node/dia-04-express-http-com-node/simpsons.js
+++ 03-back-end/bloco-22-introducao-ao-node/dia-04-express-http-com-node/simpsons.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs')
 const simpsonsUtils = require('./simpsons-utils');
+const crypto = require('crypto');
 
 const app = express()
 
@@ -9,12 +10,16 @@ app.use(express.json())
 // const simpsonsArr = JSON.parse(fs.readFileSync('simpsons.json').toString())
 
 app.get('/simpsons', async (req, res) => {
+  const token = req.headers.authorization
+  if (!token || token.length !== 16) return res.status(401).json({message: 'Token inválido'})
   const simpsons = await simpsonsUtils.getSimpsons()
   res.status(200).json(simpsons)
 })
 
 //test
 app.get('/simpsons/:id', async (req, res) => {
+  const token = req.headers.authorization
+  if (!token || token.length !== 16) return res.status(401).json({message: 'Token inválido'})
   const { id } = req.params
   const simpsons = await simpsonsUtils.getSimpsons()
   const character = simpsons.find((simp) => simp.id === id)
@@ -23,6 +28,8 @@ app.get('/simpsons/:id', async (req, res) => {
 })
 
 app.post('/simpsons', async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token || token.length !== 16) return res.status(401).json({message: 'Token inválido'})
   const { id, name} = req.body
   const simpsons = await simpsonsUtils.getSimpsons()
   const characterExists = simpsons.find((simp) => simp.id === id)
@@ -30,6 +37,23 @@ app.post('/simpsons', async (req, res) => {
   simpsons.push({id, name})
   await simpsonsUtils.setSimpsons(simpsons)
   res.status(204).end()
+})
+
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
+
+
+app.post('/signup', (req, res) => {
+  const {email, password, firstName, phone} = req.body
+
+  if (!email || !password || !firstName || !phone) {
+    return res.status(401).json({message: 'missing fileds'})
+  }
+
+  const token = generateToken()
+
+  res.status(200).json(token)
 })
 
 app.listen(2000, () => {
