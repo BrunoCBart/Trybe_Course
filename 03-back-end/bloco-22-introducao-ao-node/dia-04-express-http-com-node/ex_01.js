0bc6eec1a7903e819bc97b304645541cce9fbd94
Bruno Bartolomeu <bruno.cbartlomeu@gmail.com>
2022-02-12 08:53:42 -0300

    ex_02

--- 03-back-end/bloco-22-introducao-ao-node/dia-04-express-http-com-node/ex_01.js
+++ 03-back-end/bloco-22-introducao-ao-node/dia-04-express-http-com-node/ex_01.js
@@ -2,9 +2,17 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 
 app.get('/ping', (req, res) => {
   res.status(200).json({message: 'pong'})
+})
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body
+  res.status(200).json({message: `Hello ${name}`})
+
 })
 
 app.listen(5000, () => {
